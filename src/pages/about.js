dcf489d2bdee852f25b2d7b7ac6dea5e69d1a157
PShivers <paulsshivers@gmail.com>
2023-05-15 12:50:10 -0400

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const AboutPage = () => {
+	return (
+		<main>
+			<h1>About Me</h1>
+			<Link to="/">Back to Home</Link>
+			<p>
+				Hi there! I'm the proud creator of this site, which I built with Gatsby.
+				This is my first time working with Gatsby so don't expect a whole lot
+				from this site.
+			</p>
+		</main>
+	);
+};
+
+export const Head = () => <title>About Me</title>;
+
+export default AboutPage;
